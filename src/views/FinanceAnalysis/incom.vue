b7720c260703f3d50f31f0345eb92c2755379393
jinjinliang <3122702689@qq.com>
2023-02-07 11:14:42 +0800

    echart1Adjust

--- src/views/FinanceAnalysis/incom.vue
+++ src/views/FinanceAnalysis/incom.vue
@@ -39,14 +39,6 @@
 
         <div class="category">
         </div>
-        <div class="ico">
-          <div class="actulIcon"></div>
-          <span class="actulText">实际</span>
-          <div class="caculateIcon"></div>
-          <span class="caculateText">预算</span>
-          <div class="lastYearIcon"></div>
-          <span class="lastYearText">去年同期</span>
-        </div>
 
         <!-- echarts1 -->
         <fareEcharts />
@@ -220,162 +212,96 @@ export default {
     this.getIncomMonthTable()
   },
   mounted() {
-    var app = {};
     var chartDom = document.getElementById('main');
     var myChart = echarts.init(chartDom);
     var option;
-    const posList = [
-      'left',
-      'right',
-      'top',
-      'bottom',
-      'inside',
-      'insideTop',
-      'insideLeft',
-      'insideRight',
-      'insideBottom',
-      'insideTopLeft',
-      'insideTopRight',
-      'insideBottomLeft',
-      'insideBottomRight'
-    ];
-    app.configParameters = {
-      align: {
-        options: {
-          left: 'left',
-          center: 'center',
-          right: 'right'
-        }
-      },
-      verticalAlign: {
-        options: {
-          top: 'top',
-          middle: 'middle',
-          bottom: 'bottom'
-        }
-      },
-      position: {
-        options: posList.reduce(function (map, pos) {
-          map[pos] = pos;
-          return map;
-        }, {})
-      },
-      distance: {
-        min: 0,
-        max: 100
-      }
-    };
-    app.config = {
-      rotate: 90,
-      align: 'left',
-      verticalAlign: 'middle',
-      position: 'insideBottom',
-      distance: 15,
-      onChange: function () {
-        const labelOption = {
-          rotate: app.config.rotate,
-          align: app.config.align,
-          verticalAlign: app.config.verticalAlign,
-          position: app.config.position,
-          distance: app.config.distance
-        };
-        myChart.setOption({
-          series: [
-            {
-              label: labelOption
-            },
-            {
-              label: labelOption
-            },
-            {
-              label: labelOption
-            },
-            {
-              label: labelOption
-            }
-          ]
-        });
-      }
-    };
-    const labelOption = {
-      show: false,
-      position: app.config.position,
-      distance: app.config.distance,
-      align: app.config.align,
-      verticalAlign: app.config.verticalAlign,
-      rotate: app.config.rotate,
-      formatter: '{c}  {name|{a}}',
-      fontSize: 16,
-      rich: {
-        name: {}
-      }
-    };
+
     option = {
+      title: {
+        text: '各月份营收费用瀑布图'
+      },
       tooltip: {
         trigger: 'axis',
         axisPointer: {
           type: 'shadow'
+        },
+        formatter: function (params) {
+          let tar;
+          if (params[1] && params[1].value !== '-') {
+            tar = params[1];
+          } else {
+            tar = params[2];
+          }
+          return tar && tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
         }
       },
       legend: {
-        data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+        data: ['实际', '预算', '去年同期']
       },
-      toolbox: {
-        show: false,
-        orient: 'vertical',
-        left: 'right',
-        top: 'center',
-        feature: {
-          mark: { show: true },
-          dataView: { show: true, readOnly: false },
-          magicType: { show: true, type: ['line', 'bar', 'stack'] },
-          restore: { show: true },
-          saveAsImage: { show: true }
-        }
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
       },
-      xAxis: [
-        {
-          type: 'category',
-          axisTick: { show: false },
-          data: ['收入', '成本', '毛利', '营业费用', '管理费用', '财务费用', '利润']
-        }
-      ],
-      yAxis: [
-        {
-          type: 'value'
-        }
-      ],
+      xAxis: {
+        type: 'category',
+        data: ['收入', '成本', '毛利', '营业费用', '管理费用', '财务费用', '利润']
+      },
+      yAxis: {
+        type: 'value'
+      },
       series: [
         {
-          name: '实际',
+          name: 'Placeholder',
           type: 'bar',
-          barGap: 0,
-          label: labelOption,
-          emphasis: {
-            focus: 'series'
+          stack: 'Total',
+          silent: true,
+          itemStyle: {
+            borderColor: 'transparent',
+            color: 'transparent'
           },
-          data: [320, 332, 301, 334, 390, 500]
-        },
-        {
-          name: '预算',
-          type: 'bar',
-          label: labelOption,
           emphasis: {
-            focus: 'series'
+            itemStyle: {
+              borderColor: 'transparent',
+              color: 'transparent'
+            }
           },
-          data: [220, 182, 191, 234, 290, 43]
+          data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1689, 1856, 1495, 1292]
         },
         {
           name: '去年同期',
           type: 'bar',
-          label: labelOption,
-          emphasis: {
-            focus: 'series'
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'top'
           },
-          data: [150, 232, 201, 154, 190, 321]
+          data: [900, 345, 393, 135, 178, 286]
+        },
+        {
+          name: '预算',
+          type: 'bar',
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'top'
+          },
+          data: [900, 345, 393, '-', '-', 135, 178, 286, '-', '-', '-']
+        },
+        {
+          name: '实际',
+          type: 'bar',
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'bottom'
+          },
+          data: [108, 154, 119, 361, 203]
         }
       ]
     };
+
     option && myChart.setOption(option);
   },
 
