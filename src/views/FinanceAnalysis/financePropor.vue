a775a65696baa5b3bcc72e3a681d1c90bcb4f491
jinjinliang <3122702689@qq.com>
2023-02-08 11:49:32 +0800

    lunchTime

--- src/views/FinanceAnalysis/financePropor.vue
+++ src/views/FinanceAnalysis/financePropor.vue
@@ -35,7 +35,23 @@
     <a-row>
       <a-col :span="12">
         col-12
-        <operationCapibilityVue />
+        <a-table :columns="columns" :data-source="data" :pagination='false'>
+          <a slot="name" slot-scope="text">{{ text }}</a>
+          <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+          <span slot="tags" slot-scope="tags">
+            <a-tag v-for="tag in tags" :key="tag"
+              :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'">
+              {{ tag.toUpperCase() }}
+            </a-tag>
+          </span>
+          <span slot="action" slot-scope="text, record">
+            <a>Invite 一 {{ record.name }}</a>
+            <a-divider type="vertical" />
+            <a>Delete</a>
+            <a-divider type="vertical" />
+            <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+          </span>
+        </a-table>
       </a-col>
       <a-col :span="12">
         col-12
@@ -53,11 +69,69 @@ import operationCapibilityVue from './components/operationCapibility.vue.vue';
 import profitEchart from './components/profitEchart.vue';
 import investmentPayback from './components/investmentPayback.vue';
 import InvestmentPayback from './components/investmentPayback.vue';
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+];
 export default {
   components: { shortTerm, longTerm, operationCapibilityVue, profitEchart, InvestmentPayback },
   data() {
     return {
+
+      data,
+      columns,
       buttonIndex: 2
+
     }
   },
   methods: {
