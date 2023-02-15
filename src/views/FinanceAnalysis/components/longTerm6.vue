9834d63750bb8f28eef2ccf68c66557723ddf2b1
jinjinliang <3122702689@qq.com>
2023-02-15 17:30:29 +0800

    workFinish

--- src/views/FinanceAnalysis/components/longTerm6.vue
+++ src/views/FinanceAnalysis/components/longTerm6.vue
@@ -77,60 +77,7 @@ export default {
     this.initLongTerm()
   },
   mounted() {
-    // var chartDom = document.getElementById('main2');
-    // var myChart = echarts.init(chartDom);
-    // var option;
 
-    // option = {
-    //   title: {
-    //     text: '长期偿债能力'
-    //   },
-    //   tooltip: {
-    //     trigger: 'axis'
-    //   },
-    //   legend: {
-    //     data: ['前年', '去年', '今年']
-    //   },
-    //   grid: {
-    //     left: '3%',
-    //     right: '4%',
-    //     bottom: '3%',
-    //     containLabel: true
-    //   },
-    //   toolbox: {
-    //     feature: {}
-    //   },
-    //   xAxis: {
-    //     type: 'category',
-    //     boundaryGap: false,
-    //     data: ['1月', '2月', '3月', '4月', '5月', '6月']
-    //   },
-    //   yAxis: {
-    //     type: 'value'
-    //   },
-    //   series: [
-    //     {
-    //       name: '前年',
-    //       type: 'line',
-    //       stack: 'Total',
-    //       data: [120, 132, 101, 134, 90, 230, 210]
-    //     },
-    //     {
-    //       name: '去年',
-    //       type: 'line',
-    //       stack: 'Total',
-    //       data: [220, 182, 191, 234, 290, 330, 310]
-    //     },
-    //     {
-    //       name: '今年',
-    //       type: 'line',
-    //       stack: 'Total',
-    //       data: [150, 232, 201, 154, 190, 330, 410]
-    //     }
-    //   ]
-    // };
-
-    // option && myChart.setOption(option, true);
   },
 }
 </script>
